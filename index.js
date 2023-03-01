e6f06750dc6913b5dc80c419c16c62fcc39fc1a1
Sergey-Kali <110811657+Sergey-Kali@users.noreply.github.com>
2023-03-01 15:47:29 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const firstNumber = prompt('Введіть перше число:');
+const secondNumber = prompt('Введіть друге чило');
+
+alert(`Сумма введених чисел: ${Number(firstNumber) + Number(secondNumber)}`);
+alert(`Різниця введених чисел: ${firstNumber - secondNumber}`);
+alert(`Множення введених чисел: ${firstNumber * secondNumber}`);
+alert(`Ділення введених чисел: ${firstNumber / secondNumber}`);
+alert(`Остача від ділення введених чисел: ${firstNumber % secondNumber}`);
