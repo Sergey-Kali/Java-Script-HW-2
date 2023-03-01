64eea407ea43e82e1a84175ebf79cd2dec01fa70
Sergey-Kali <110811657+Sergey-Kali@users.noreply.github.com>
2023-03-01 17:28:10 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-const firstNumber = prompt('Введіть перше число:');
-const secondNumber = prompt('Введіть друге чило');
+const firstRequest = prompt("Введіть перше число:");
+const firstNumber = Number(firstRequest);
 
-alert(`Сумма введених чисел: ${Number(firstNumber) + Number(secondNumber)}`);
-alert(`Різниця введених чисел: ${firstNumber - secondNumber}`);
-alert(`Множення введених чисел: ${firstNumber * secondNumber}`);
-alert(`Ділення введених чисел: ${firstNumber / secondNumber}`);
-alert(`Остача від ділення введених чисел: ${firstNumber % secondNumber}`);
+if (firstNumber >= 0) {
+  const secondRequest = prompt("Введіть друге чило");
+  const secondNumber = Number(secondRequest);
+
+  if (secondNumber >= 0) {
+    alert(`Сумма введених чисел: ${firstNumber + secondNumber}`);
+    if (firstNumber < secondNumber) {
+      const message = confirm("Ви впевнені, що хочете продовжити операцію?");
+      if (message) {
+        alert(`Різниця введених чисел: ${firstNumber - secondNumber}`);
+      }
+    } else {
+      alert(`Різниця введених чисел: ${firstNumber - secondNumber}`);
+    }
+
+    alert(`Множення введених чисел: ${firstNumber * secondNumber}`);
+
+    if (secondNumber === 0) {
+      alert("Помилка! Друге число 0, на 0 ділити не можна");
+    } else {
+      alert(`Ділення введених чисел: ${firstNumber / secondNumber}`);
+    }
+  } else {
+    alert("Помилка! Ви не ввели друге число!");
+  }
+} else {
+  alert("Помилка! Ви не ввели число!");
+}
